d0c3e2b858ef8d887d4f2662a0c23b694ecfac0e
Fernando Silva <fernandosilv4c@gmail.com>
2017-04-22 00:02:07 +0200

    Fichero de prueba

--- Test/prueba.cpp
+++ Test/prueba.cpp
@@ -3,9 +3,47 @@
 
 #define PRUEBA 1
 
+//Comentario en una Linea
+
+/*
+ * COmentario en varias lineas
+ *
+ *
+ *
+ *
+ * */
+
+void prueba{
+
+	if(true){
+	
+	
+	
+	}
+
+
+	do{
+	
+	
+
+	}while();
+
+	while(){
+	
+	
+	};
+	
+
+}
+
 
 int main(){
 
+	bool opcion = true;
+	char letra= 'a'
+	"asdasdasd"
+
+	prueba();
 
 
 
